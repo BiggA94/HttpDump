e9d8febb4eea0f82652e29f938438f1d993047db
Alexander Weidt <alexw94ks@yahoo.de>
2019-04-02 16:17:34 +0200

    reformat

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -2,37 +2,43 @@ package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.http.HttpServerRequest;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
-    var options = new HttpServerOptions().setLogActivity(true);
+    var options = new HttpServerOptions()
+      .setUseAlpn(true)
+      .setSsl(true)
+      .setLogActivity(true);
 
 
     vertx.createHttpServer(options)
-      .requestHandler(req -> {
-        StringBuffer stringBuffer = new StringBuffer();
-        append(stringBuffer, "#################");
-        append(stringBuffer, "New Request");
-        append(stringBuffer, "###### Path:");
-        append(stringBuffer, req.path());
-        append(stringBuffer, "###### Headers:");
-        append(stringBuffer, req.headers().toString());
-        append(stringBuffer, "###### Body:");
-        req.bodyHandler(buff -> {
-          append(stringBuffer, buff.toString());
-          append(stringBuffer, "#################");
-          req.response().end(stringBuffer.toString());
-          System.out.println(stringBuffer.toString());
-        });
-      })
+      .requestHandler(this::handle)
       .listen(8080);
   }
 
   public void append(StringBuffer stringBuffer, String string) {
     stringBuffer.append(string);
     stringBuffer.append('\n');
+  }
+
+  public void handle(HttpServerRequest req) {
+    StringBuffer stringBuffer = new StringBuffer();
+    append(stringBuffer, "#################");
+    append(stringBuffer, "New Request");
+    append(stringBuffer, "###### Path:");
+    append(stringBuffer, req.path());
+    append(stringBuffer, "###### Headers:");
+    append(stringBuffer, req.headers().toString());
+    append(stringBuffer, "###### Body:");
+    req.bodyHandler(buff -> {
+      append(stringBuffer, buff.toString());
+      append(stringBuffer, "#################");
+      req.response().end(stringBuffer.toString());
+      System.out.println(stringBuffer.toString());
+    });
   }
 
 }
